6fc2dc8c346837ca4a51ed08463fe52c658d8e87
SoniaSon <48282768+SoniaSon@users.noreply.github.com>
2020-09-22 16:18:32 +0200

    Add files via upload

---
+++ Personne.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Personnes
+{
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+        private static int nbPersonnes;
+
+        static Personne()
+        {
+            nbPersonnes = 0;
+        }
+
+
+        public Personne(string nom, string prenom, int age)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            nbPersonnes++;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("[Affichager les informations de la personne]");
+            Console.WriteLine("Nom : " + nom);
+            Console.WriteLine("Prénom : " + prenom);
+            Console.WriteLine("Age : " + age);
+        }
+
+        public int Combien()
+        {
+            return nbPersonnes;
+        }
+    }
+}
